8727bfb3d9debdf91ec0633616748cf0ac8cc3a9
jiseok419 <pyosik0419@gmail.com>
2023-09-25 11:32:45 +0900

    Initial Commit

---
+++ TodaySalt/TodaySaltApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaySaltApp.swift
+//  TodaySalt
+//
+//  Created by 강지석 on 2023/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TodaySaltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
